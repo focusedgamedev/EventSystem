59b47d6bf3535fff5a1561ee6205e656e06db5fb
focusedgamedev <team@thefocusedgamedev.com>
2020-12-19 11:20:16 -0600

    Initial Content Commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # EventSystem
 Simple Event System
+
+## Example Usage
+
+```
+using System;
+using SimpleEvents;
+
+namespace EventSystemWorking
+{
+		public class StringEventData
+		{
+				private StringEventData() { }
+				public StringEventData(string text)
+				{
+						m_text = text;
+				}
+
+				string m_text;
+
+				public string String
+				{
+						get { return m_text; }
+						private set { }
+				}
+		}
+
+		public class Test
+		{
+				public void OnStringData(StringEventData data)
+				{
+						Console.WriteLine("String Event! - " + data.String );
+				}
+
+				public void RunTest()
+				{
+						EventSystem eventSystem = new EventSystem();
+						eventSystem.Register<StringEventData>(OnStringData);
+
+						StringEventData data = new StringEventData("This is a string...");
+
+						eventSystem.Dispatch(data);
+						eventSystem.Deregister<StringEventData>(OnStringData);
+				}
+		}
+
+		class Program
+		{
+				static void Main(string[] args)
+				{
+						Test test = new Test();
+						test.RunTest();
+						Console.WriteLine("Done...");
+				}
+		}
+}
+```
+
---
+++ src/EventSystem.cs
@@ -0,0 +1,116 @@
+﻿//#define VERBOSE_LOGGING
+
+using System;
+using System.Collections.Generic;
+
+namespace SimpleEvents
+{
+    public interface IEvent { }
+
+    public class BaseEvent<T> : IEvent
+    {
+        public BaseEvent()
+        {
+            m_listeners = new List<T>();
+        }
+
+        public void AddListener( T listener )
+        {
+            m_listeners.Add( listener );
+        }
+
+        public void RemoveListener( T listener )
+        {
+            m_listeners.Remove( listener );
+        }
+
+        protected List<T> m_listeners;
+    }
+
+    public class Event<T> : BaseEvent<Action<T>>
+    {
+        public void Dispatch( T arg )
+        {
+            for ( int i = 0; i < m_listeners.Count; i++ )
+            {
+                m_listeners[i].Invoke( arg );
+            }
+        }
+    }
+
+    public class EventSystem
+    {
+        static EventSystem m_instance;
+        public static EventSystem Instance
+        {
+            get
+            {
+                return m_instance;
+            }
+            private set { }
+        }
+
+				public EventSystem()
+				{
+						m_instance = this;
+				}
+
+        Dictionary<Type, IEvent> m_eventHandlers = new Dictionary<Type, IEvent>();
+
+        public void Register<T>( Action<T> evt )
+        {
+            IEvent ievent;
+            Event<T> _event;
+            if ( m_eventHandlers.TryGetValue( typeof(T), out ievent ) )
+            {
+								_event = ievent as Event<T>;
+                if (_event == null)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            else
+            {
+								_event = new Event<T>();
+                m_eventHandlers.Add( typeof(T), _event);
+            }
+
+						_event.AddListener(evt);
+        }
+
+        public void Deregister<T>(Action<T> evt)
+        {
+            IEvent ievent;
+            if ( m_eventHandlers.TryGetValue( typeof(T), out ievent ) )
+            {
+                Event<T> _event = ievent as Event<T>;
+                if (_event == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                _event.RemoveListener(evt);
+            }
+        }
+
+        public void Dispatch<T>( T arg )
+        {
+            Dispatch<T>( typeof(T), arg );
+        }
+
+        public void Dispatch<T>( Type signalKey, T arg )
+        {
+            IEvent _evt;
+            if ( m_eventHandlers.TryGetValue( signalKey, out _evt ) )
+            {
+                Event<T> _event = _evt as Event<T>;
+                if (_event == null)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                _event.Dispatch(arg);
+            }
+        }
+    }
+}
